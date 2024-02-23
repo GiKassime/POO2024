46bb8c1119d3d37f53297b586a001848153fa788
lab <>
2024-02-23 15:43:59 -0300

    .

---
+++ Exercicios_1_Bimestre/aula_3/trabalho/primos.php
@@ -0,0 +1,13 @@
+<?php
+
+function numeroPrimo(int $num){
+    if($num%$num == 0 & $num %1 == 0){
+        return true;
+    }else{
+        return false;
+    }
+}
+
+do {
+    # code...
+} while ($resposta < );
