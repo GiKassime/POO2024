05937682591163d72bf79c2fa0feece3f3669812
Kassime <134729407+GiKassime@users.noreply.github.com>
2024-02-24 18:39:05 -0300

    ,

--- Exercicios_1_Bimestre/aula_3/trabalho/primos.php
+++ Exercicios_1_Bimestre/aula_3/trabalho/primos.php
@@ -1,13 +1,28 @@
 <?php
 
-function numeroPrimo(int $num){
-    if($num%$num == 0 & $num %1 == 0){
-        return true;
-    }else{
+function numeroPrimo(int $num) : bool{
+    if(($num%2 != 0 || $num == 2) && $num > 1)//o único número primo par é o 2  
+    return true;      
+    else
         return false;
-    }
+    
 }
+function menu(){
+    echo "*********PRIMO OU NÃO?**********\n";
+}
+menu();
+do{
+    do {
+        $resposta = readline("\nDigite um número:");
+    } while (is_numeric($resposta) == false);//Caso o usuário não digite um número ou nada
+  
+   
+        
+  
+    if(numeroPrimo($resposta))
+        echo "$resposta é um número primo\n";
+    else
+        echo "$resposta não é um número primo\n";
 
-do {
-    # code...
-} while ($resposta < );
+    
+}while($resposta > 1);
