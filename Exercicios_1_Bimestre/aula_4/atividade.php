4fde9d054954b9897b13ef5a9a3c75a56cdfbf54
Giovana Kassime <giovana.kassime.tds2023@gmail.com>
2024-03-07 14:02:00 -0300

    ;

---
+++ Exercicios_1_Bimestre/aula_4/atividade.php
@@ -0,0 +1,22 @@
+<?php
+function numeroPrimo(int $num) : bool{
+    $divisor = 0;
+    for ($i=1; $i <= $num; $i++) { 
+        if($num%$i==0){
+            $divisor++;
+        }
+    }
+    return($divisor == 2);          
+}
+$vetor = array();
+$num = 1;
+while($num!= 0){
+    $num = readline("\nDigite um número: ");
+    if(numeroPrimo($num)){
+        print("\n$num é primo!\n");
+        array_push($vetor,$num);
+    }else{
+        print("\n$num não é primo!\n");
+    }
+}
+print_r($vetor);
